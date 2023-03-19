bbeaa7c2fbb584bff14de654f4b70f52d2c33f08
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-03-19 04:06:03 -0300

    Teste de inserir dados na database

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,10 +1,17 @@
-class HomerController {
-  index(res, req) {
-    req.json({
-      tudoCerto: true,
-    });
+import Aluno from '../models/aluno';
 
-  }
+class HomerController {
+  async index(res, req) {
+    const novoAluno = await Aluno.create({
+      nome: 'Josefá',
+      email: 'fafa@gmail.com',
+      idade: 22,
+      peso: 100,
+      altura: 1.90,
+    })
+    req.json(novoAluno);
+
+  };
 }
 
 export default new HomerController();
