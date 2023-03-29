95a54fc19c17265bcbb8a175a277bcbd5f1fee9d
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-03-29 16:27:15 -0300

    Commit

---
+++ pull-server.sh
@@ -0,0 +1,11 @@
+npm run build
+git add .
+
+read message
+echo "Mensagem do commit: "
+git commit -am "$message"
+git push
+ssh 35.198.3.183 \
+  'git -C /home/Leu/api/ ' \
+  'pull origin master && ' \
+  'pm2 restart api && sudo systemctl restart nginx'
