e2746a3bd00f8d753ce11cb0dc5312fa9a7a91b8
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-04-19 15:22:41 -0300

    ad

--- dist/app.js
+++ dist/app.js
@@ -15,7 +15,6 @@ var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopReq
 const whiteList = [
   'http://35.198.3.183',
   'http://localhost:3000',
-  'http://localhost:3001',
 ];
 
 const corsOptions = {
